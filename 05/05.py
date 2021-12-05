9f687c2b7b230d24d15114ce21a490f8aab0dadd
Phil Kendall <philip@shadowmagic.org.uk>
2021-12-05 05:54:56 +0000

    Tidy.

--- 05/05.py
+++ 05/05.py
@@ -1,32 +1,27 @@
 #!/usr/bin/env python3
 
 import collections
+import math
 import sys
-
-import numpy as np
 
 def parse_vent(text):
     a, _, b = text.split()
     x1, y1 = a.split(',')
     x2, y2 = b.split(',')
-    return (int(x1), int(y1), int(x2), int(y2))
+    return [int(v) for v in [x1, y1, x2, y2]]
 
 def get_step(v1, v2):
-    return 1 if v2 > v1 else (-1 if v2 < v1 else 0)
+    step = 0 if v1 == v2 else math.copysign(1, v2-v1)
+    return (step, v2 + step) if step else (0, None)
 
 with open(sys.argv[1]) as f:
-    lines = f.readlines()
-
-vents = [parse_vent(t) for t in lines]
+    vents = [parse_vent(l) for l in f.readlines()]
 
 data1 = collections.defaultdict(lambda: 0)
 data2 = collections.defaultdict(lambda: 0)
-for v in vents:
-    x1, y1, x2, y2 = v
-    xstep = get_step(x1, x2)
-    ystep = get_step(y1, y2)
-    xend = x2 + xstep if xstep else None
-    yend = y2 + ystep if ystep else None
+for x1, y1, x2, y2 in vents:
+    xstep, xend = get_step(x1, x2)
+    ystep, yend = get_step(y1, y2)
 
     x = x1
     y = y1
