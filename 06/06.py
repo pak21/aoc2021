e1184f3055e664e968e56f83b4b1010fe6065a5d
Phil Kendall <philip@shadowmagic.org.uk>
2021-12-07 10:14:04 +0000

    Minor tidy.

--- 06/06.py
+++ 06/06.py
@@ -3,19 +3,16 @@
 import collections
 import sys
 
-import numpy as np
-
 with open(sys.argv[1]) as f:
     days = collections.Counter([int(x) for x in f.readline().split(',')])
 
 n = int(sys.argv[2])
 
 for i in range(n):
-    x = {(a-1): b for a, b in days.items()}
-    x[6] = x.get(6, 0) + x.get(-1, 0)
-    x[8] = x.get(-1, 0)
-    if -1 in x:
-        del x[-1]
-    days = x
+    days = {(a-1): b for a, b in days.items()}
+    days[6] = days.get(6, 0) + days.get(-1, 0)
+    days[8] = days.get(-1, 0)
+    if -1 in days:
+        del days[-1]
 
 print(sum(days.values()))
