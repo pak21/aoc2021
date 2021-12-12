7c032edef6e4ec7284d38448f02e119596ea37db
Phil Kendall <philip@shadowmagic.org.uk>
2021-12-12 06:30:04 +0000

    Much better.

--- 12/12.py
+++ 12/12.py
@@ -3,34 +3,54 @@
 import collections
 import sys
 
-import numpy as np
+PART2 = True
 
-with open(sys.argv[1]) as f:
-    connl = [l.strip().split('-') for l in f.readlines()]
+class State:
+    def __init__(self, path, visited, double_visit):
+        self.path = path
+        self.visited = visited
+        self.double_visit = double_visit
+
+def zero():
+    return State([], set(), False)
+
+def can_visit(state, next_cave):
+    return \
+        next_cave.isupper() or \
+        next_cave not in state.visited or \
+        (PART2 and not state.double_visit)
+
+def visit(state, next_cave):
+    return State(
+        [next_cave] + state.path,
+        state.visited | set([next_cave]),
+        state.double_visit or (state.double_visit if next_cave.isupper() else (next_cave in state.visited))
+    )
 
 conns = collections.defaultdict(list)
-for a, b in connl:
-    conns[a].append(b)
-    conns[b].append(a)
+with open(sys.argv[1]) as f:
+    for a, b in [l.strip().split('-') for l in f.readlines()]:
+        if b != 'start':
+            conns[a].append(b)
+        if a != 'start':
+            conns[b].append(a)
 
 seen = set()
-todo = [['start']]
-completed = []
+todo = [visit(zero(), 'start')]
+paths = 0
 
 while todo:
-    foo = todo.pop()
-    seen.add(tuple(foo))
+    old_state = todo.pop()
+    seen.add(old_state)
 
-    if foo[-1] == 'end':
-        completed.append(foo)
-        continue
-
-    for bar in conns[foo[-1]]:
-        if bar.islower() and bar in foo:
+    for next_cave in conns[old_state.path[0]]:
+        if next_cave == 'end':
+            paths += 1
             continue
 
-        new_state = foo + [bar]
-        if tuple(new_state) not in seen:
-            todo.append(new_state)
+        if can_visit(old_state, next_cave):
+            new_state = visit(old_state, next_cave)
+            if new_state not in seen:
+                todo.append(new_state)
 
-print(len(completed))
+print(paths)
