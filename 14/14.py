93c1be40f0ff03508d55201c0a4194fd25d74887
Phil Kendall <philip@shadowmagic.org.uk>
2021-12-14 05:46:45 +0000

    Tidy.

--- 14/14.py
+++ 14/14.py
@@ -6,19 +6,15 @@ import sys
 with open(sys.argv[1]) as f:
     template, rules_s = f.read().split('\n\n')
 
-rules_l = [x.split(' -> ') for x in rules_s.strip().split('\n')]
-rules = {a: b for a, b in rules_l}
+rules = {tuple(a): b for a, b in [x.split(' -> ') for x in rules_s.strip().split('\n')]}
 
-pairs = collections.Counter([t[0] + t[1] for t in zip(template[:-1], template[1:])])
+pairs = collections.Counter(zip(template[:-1], template[1:]))
 
 for i in range(int(sys.argv[2])):
     new_pairs = collections.defaultdict(int)
     for p, v in pairs.items():
-        if p in rules:
-            new_pairs[p[0] + rules[p]] += v
-            new_pairs[rules[p] + p[1]] += v
-        else:
-            new_pairs[p] += v
+        new_pairs[(p[0], rules[p])] += v
+        new_pairs[(rules[p], p[1])] += v
 
     pairs = new_pairs
 
@@ -27,6 +23,10 @@ for p, v in pairs.items():
     counts[p[0]] += v
     counts[p[1]] += v
 
+# Note for future Phil: every character _except_ the first and last is a part of
+# two pairs, so the above double counts everything except the first and last
+# characters. Easiest just to make everything double-counted, so we increment
+# the counts of the first and last characters by one.
 counts[template[0]] += 1
 counts[template[-1]] += 1
 
