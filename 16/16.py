2b0c04dc965aef25c6c9d318a8157e05c2ca82bd
Phil Kendall <philip@shadowmagic.org.uk>
2021-12-16 21:21:12 +0000

    Reworking.

--- 16/16.py
+++ 16/16.py
@@ -3,15 +3,36 @@
 import functools
 import sys
 
-OPERATORS = {
-    0: sum,
-    1: lambda vs: functools.reduce(lambda a, b: a * b, vs, 1),
-    2: min,
-    3: max,
-    5: lambda vs: 1 if vs[0] > vs[1] else 0,
-    6: lambda vs: 1 if vs[0] < vs[1] else 0,
-    7: lambda vs: 1 if vs[0] == vs[1] else 0,
-}
+OPERATORS = [
+    sum,
+    lambda vs: functools.reduce(lambda a, b: a * b, vs, 1),
+    min,
+    max,
+    None, # direct value handled elsewhere
+    lambda vs: 1 if vs[0] > vs[1] else 0,
+    lambda vs: 1 if vs[0] < vs[1] else 0,
+    lambda vs: 1 if vs[0] == vs[1] else 0,
+]
+
+def foo(string, start, version_sum, values):
+    start, version, value = get_packet_value(string, start)
+    return (start, version_sum + version, values + [value])
+
+def length_handler(string, start, loop_fn):
+    version_sum = 0
+    subvalues = []
+    n = 0
+
+    while loop_fn(start, n):
+        start, version_sum, subvalues = foo(string, start, version_sum, subvalues)
+        n += 1
+
+    return start, version_sum, subvalues
+    
+LENGTH_HANDLERS = [
+    (15, lambda string, start, length_value: length_handler(string, start, lambda s, _: s < start + length_value)),
+    (11, lambda string, start, length_value: length_handler(string, start, lambda _, n: n < length_value)),
+]
 
 def get_bits(string, start, offset):
     first = start // 4
@@ -23,38 +44,27 @@ def get_bits(string, start, offset):
     to_shift = 4 * (last - first + 1) - end_bits
     mask = (1 << offset) - 1
 
-    return (int(string[first:last+1], 16) >> to_shift) & mask
+    return (start + offset, (int(string[first:last+1], 16) >> to_shift) & mask)
 
 def get_packet_value(string, start):
-    version = get_bits(string, start, 3)
-    typeid = get_bits(string, start + 3, 3)
-    start += 6
+    start, version = get_bits(string, start, 3)
+    start, typeid = get_bits(string, start, 3)
 
     if typeid == 4:
         more = True
         value = 0
         while more:
-            more = get_bits(string, start, 1)
-            value = (value << 4) | get_bits(string, start + 1, 4)
-            start += 5
+            start, more = get_bits(string, start, 1)
+            start, next_value = get_bits(string, start, 4)
+            value = (value << 4) | next_value
     else:
         subvalues = []
-        length_typeid = get_bits(string, start, 1)
-        if length_typeid == 0:
-            bits_to_parse = get_bits(string, start + 1, 15)
-            start += 16
-            end_offset = start + bits_to_parse
-            while start < end_offset:
-                start, ver, v = get_packet_value(string, start)
-                version += ver
-                subvalues.append(v)
-        else:
-            packets_to_parse = get_bits(string, start + 1, 11)
-            start += 12
-            for i in range(packets_to_parse):
-                start, ver, v = get_packet_value(string, start)
-                version += ver
-                subvalues.append(v)
+        start, length_typeid = get_bits(string, start, 1)
+        length_bits, length_fn = LENGTH_HANDLERS[length_typeid]
+        start, length_value = get_bits(string, start, length_bits)
+
+        start, version_sum, subvalues = length_fn(string, start, length_value)
+        version += version_sum
 
         value = OPERATORS[typeid](subvalues)
 
