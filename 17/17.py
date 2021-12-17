0f655203536b198f738b54924a5a2897d671d7f5
Phil Kendall <philip@shadowmagic.org.uk>
2021-12-17 05:34:07 +0000

    Day 17, both parts.

---
+++ 17/17.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import collections
+import re
+import sys
+
+import numpy as np
+
+with open(sys.argv[1]) as f:
+    bounds = re.split('[^-0-9]+', f.readline().strip())
+    xmin, xmax, ymin, ymax = [int(s) for s in bounds[1:]]
+
+def simulate(vx, vy, xmin, xmax, ymin, ymax):
+    x = 0
+    y = 0
+    highest = y
+    while x < xmax and y > ymin:
+        x += vx
+        y += vy
+        vx = vx - np.sign(vx)
+        vy -= 1
+
+        if y > highest:
+            highest = y
+
+        if x >= xmin and x <= xmax and y >= ymin and y <= ymax:
+            return True, highest
+
+    return False, None
+
+part1 = None
+part2 = 0
+for vx in range(0, xmax+1):
+    for vy in range(ymin, 100): # TODO: how to set the upper limit here?
+        in_area, highest = simulate(vx, vy, xmin, xmax, ymin, ymax)
+        if in_area:
+            part2 += 1
+            if not part1 or highest > part1:
+                part1 = highest
+
+print(part1, part2)
