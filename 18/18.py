6975c4b4d0db69b94a7c3c0eef1b0334179e0ef0
Phil Kendall <philip@shadowmagic.org.uk>
2021-12-18 07:54:30 +0000

    "Tidy"

--- 18/18.py
+++ 18/18.py
@@ -9,35 +9,43 @@ class Node:
         self.right = right
         self.value = value
 
+    @staticmethod
+    def with_children(l, r):
+        return Node(l, r, None)
+
+    @staticmethod
+    def with_value(v):
+        return Node(None, None, v)
+
     @property
     def is_leaf(self):
         return isinstance(self.value, int)
 
-    def __repr__(self):
-        return str(self.value) if self.is_leaf else f'({self.left}, {self.right})'
+    @property
+    def magnitude(self):
+        return self.value if self.is_leaf else 3 * self.left.magnitude + 2 * self.right.magnitude
+
+    def set_lr(self, set_left, n):
+        if set_left:
+            self.left = n
+        else:
+            self.right = n
 
 def tree_from_list(l):
-    return Node(tree_from_list(l[0]), tree_from_list(l[1]), None) if isinstance(l, list) else Node(None, None, l)
+    return Node.with_children(tree_from_list(l[0]), tree_from_list(l[1])) if isinstance(l, list) else Node.with_value(l)
 
-def send_value(n, v, recurse_left):
-    if n.is_leaf:
-        n.value += v
-    else:
-        child = n.left if recurse_left else n.right
-        send_value(child, v, recurse_left)
+def send_value(n, v, child_fn):
+    if n:
+        if n.is_leaf:
+            n.value += v
+        else:
+            send_value(child_fn(n), v, child_fn)
 
 def explode(x, parent, is_left, to_left, to_right, depth):
     if depth == 4:
-        if to_left:
-            send_value(to_left, x.left.value, False)
-        if to_right:
-            send_value(to_right, x.right.value, True)
-
-        if is_left:
-           parent.left = Node(None, None, 0)
-        else:
-           parent.right = Node(None, None, 0)
-
+        send_value(to_left, x.left.value, lambda n: n.right)
+        send_value(to_right, x.right.value, lambda n: n.left)
+        parent.set_lr(is_left, Node.with_value(0))
         return True
 
     if not x.left.is_leaf:
@@ -45,21 +53,13 @@ def explode(x, parent, is_left, to_left, to_right, depth):
         if done:
             return True
 
-    if not x.right.is_leaf:
-        done = explode(x.right, x, False, x.left, to_right, depth + 1)
-        if done:
-            return True
-
-    return False
+    return not x.right.is_leaf and explode(x.right, x, False, x.left, to_right, depth + 1)
 
 def split(x, parent, is_left):
     if x.is_leaf:
         if x.value >= 10:
-            n = Node(Node(None, None, x.value // 2), Node(None, None, (x.value + 1) // 2), None)
-            if is_left:
-                parent.left = n
-            else:
-                parent.right = n
+            n = Node.with_children(Node.with_value(x.value // 2), Node.with_value((x.value + 1) // 2))
+            parent.set_lr(is_left, n)
             return True
         return False
 
@@ -67,14 +67,10 @@ def split(x, parent, is_left):
     if split_left:
         return True
 
-    split_right = split(x.right, x, False)
-    if split_right:
-        return True
-
-    return False
+    return split(x.right, x, False)
 
 def add(a, b):
-    c = Node(a, b, None)
+    c = Node.with_children(a, b)
 
     done = False
     while not done:
@@ -86,27 +82,20 @@ def add(a, b):
     return c
 
 def magnitude(a):
-    return a.value if a.is_leaf else 3*magnitude(a.left) + 2*magnitude(a.right)
+    return a.value if a.is_leaf else 3 * magnitude(a.left) + 2 * magnitude(a.right)
 
 with open(sys.argv[1]) as f:
-    lines = f.readlines()
+    lists = [eval(l) for l in f.readlines()]
 
-numbers = [tree_from_list(eval(l.strip())) for l in lines]
+print(functools.reduce(add, [tree_from_list(l) for l in lists]).magnitude)
 
-result = functools.reduce(add, numbers)
-
-print(magnitude(result))
-
-result2 = 0
-for i in range(len(numbers)):
-    for j in range(len(numbers)):
+part2 = 0
+for i, a in enumerate(lists):
+    for j, b in enumerate(lists):
         if i == j:
             continue
-        a = tree_from_list(eval(lines[i].strip()))
-        b = tree_from_list(eval(lines[j].strip()))
-        c = add(a, b)
-        d = magnitude(c)
-        if d > result2:
-            result2 = d
+        m = add(tree_from_list(a), tree_from_list(b)).magnitude
+        if m > part2:
+            part2 = m
 
-print(result2)
+print(part2)
