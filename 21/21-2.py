a93766bc24952a92f728865d4cf4c7c09a5cb10d
Phil Kendall <philip@shadowmagic.org.uk>
2021-12-21 10:12:55 +0000

    Move to namedtuples for clarity.

--- 21/21-2.py
+++ 21/21-2.py
@@ -4,23 +4,29 @@ import collections
 import itertools
 import sys
 
-MOVES = collections.Counter([sum(x) for x in itertools.product(range(1, 4), repeat=3)])
+NUM_DICE = 3
+DICE_SIZE = 3
+WINNING_SCORE = 21
+
+PlayerState = collections.namedtuple('PlayerState', ['position', 'score'])
+GameState = collections.namedtuple('GameState', ['playerstates', 'next'])
+
+MOVES = collections.Counter([sum(dice) for dice in itertools.product(range(1, DICE_SIZE+1), repeat=NUM_DICE)])
 
 def next_state(state, roll):
-    new_pos = (state[0][state[1]][0] + roll - 1) % 10 + 1
-    new_score = state[0][state[1]][1] + new_pos
+    ps = state.playerstates[state.next]
+    new_pos = (ps.position + roll - 1) % 10 + 1
+    new_score = ps.score + new_pos
 
-    if state[1] == 0:
-        return (((new_pos, new_score), state[0][1]), 1)
+    if state.next:
+        return (state.playerstates[0], PlayerState(new_pos, new_score))
     else:
-        return ((state[0][0], (new_pos, new_score)), 0)
+        return (PlayerState(new_pos, new_score), state.playerstates[1])
 
 with open(sys.argv[1]) as f:
-    positions = tuple([int(l.strip().split()[4]) for l in f.readlines()])
+    player_states = tuple([PlayerState(int(l.strip().split()[-1]), 0) for l in f.readlines()])
 
-player_states = tuple([(p, 0) for p in positions])
-
-states = {(player_states, 0): 1}
+states = {GameState(player_states, 0): 1}
 
 win_counts = [0, 0]
 while states:
@@ -29,10 +35,10 @@ while states:
         evolved_states = {next_state(old_state, roll): count for roll, count in MOVES.items()}
         for evolved_state, evolved_count in evolved_states.items():
             new_count = old_count * evolved_count
-            if evolved_state[0][1 - evolved_state[1]][1] >= 21:
-                win_counts[1 - evolved_state[1]] += new_count
+            if evolved_state[old_state.next].score >= WINNING_SCORE:
+                win_counts[old_state.next] += new_count
             else:
-                new_states[evolved_state] += new_count
+                new_states[GameState(evolved_state, 1 - old_state.next)] += new_count
 
     states = new_states
 
