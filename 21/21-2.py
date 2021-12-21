d4f870fd89afd3ca2cc9c9e20f0ca6796acd90d8
Phil Kendall <philip@shadowmagic.org.uk>
2021-12-21 06:30:42 +0000

    Tidy.

--- 21/21-2.py
+++ 21/21-2.py
@@ -6,35 +6,28 @@ import sys
 
 MOVES = collections.Counter([sum(x) for x in itertools.product(range(1, 4), repeat=3)])
 
-def evolve(state):
-    old_pos = state[0][state[2]]
-    old_score = state[1][state[2]]
-    new_states = {}
-    for roll, count in MOVES.items():
-        new_pos = (old_pos + roll - 1) % 10 + 1
-        new_score = old_score + new_pos
+def next_state(state, roll):
+    new_pos = (state[0][state[2]] + roll - 1) % 10 + 1
+    new_score = state[1][state[2]] + new_pos
 
-        if state[2] == 0:
-            new_state = ((new_pos, state[0][1]), (new_score, state[1][1]), 1)
-        else:
-            new_state = ((state[0][0], new_pos), (state[1][0], new_score), 0)
+    if state[2] == 0:
+        new_state = ((new_pos, state[0][1]), (new_score, state[1][1]), 1)
+    else:
+        new_state = ((state[0][0], new_pos), (state[1][0], new_score), 0)
 
-        new_states[new_state] = count
-
-    return new_states
+    return new_state
 
 with open(sys.argv[1]) as f:
     positions = tuple([int(l.strip().split()[4]) for l in f.readlines()])
 
-scores = tuple([0] * len(positions))
-
-states = {(positions, scores, 0): 1}
+states = {(positions, (0, 0), 0): 1}
 
 win_counts = [0, 0]
 while states:
     new_states = collections.defaultdict(int)
     for old_state, old_count in states.items():
-        for evolved_state, evolved_count in evolve(old_state).items():
+        evolved_states = {next_state(old_state, roll): count for roll, count in MOVES.items()}
+        for evolved_state, evolved_count in evolved_states.items():
             new_states[evolved_state] += old_count * evolved_count
 
     states = new_states
