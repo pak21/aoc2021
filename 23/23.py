f46392f807bae71edcc54d9b071d54f61e26ecca
Phil Kendall <philip@shadowmagic.org.uk>
2021-12-23 12:22:56 +0000

    Further tidy.

--- 23/23.py
+++ 23/23.py
@@ -5,8 +5,6 @@ import sys
 
 CREATURE_TYPES = 4
 
-VALID_HALLWAY_X = set([1, 2, 4, 6, 8, 10, 11])
-
 def creature_x(creature_type):
     return 3 + 2 * creature_type
 
@@ -42,7 +40,7 @@ def valid_moves(creature_type, creature_map, creature_count, x0, y0):
             if (x0, y1) in creature_positions:
                 return
 
-        for x1 in VALID_HALLWAY_X:
+        for x1 in [1, 2, 4, 6, 8, 10, 11]:
             if check_hallway(x0, x1, creature_positions):
                 yield (x1, 1)
 
@@ -52,13 +50,17 @@ def move_cost(old, new, creature_type):
 with open(sys.argv[1]) as f:
     burrow = [list(l) for l in f.readlines()]
 
-creatures = tuple(sorted([(ord(c) - 65, (x, y)) for y, row in enumerate(burrow) for x, c in enumerate(row) if c >= 'A' and c <= 'D']))
+creatures = tuple(sorted([
+    (ord(c) - 65, (x, y))
+    for y, row in enumerate(burrow)
+    for x, c in enumerate(row)
+    if c >= 'A' and c <= 'D'
+]))
 creature_count = max([y for _, (_, y) in creatures]) - 1
 
 final_state = tuple([(c, (creature_x(c), y)) for c in range(CREATURE_TYPES) for y in range(2, creature_count+2)])
 
-initial_state = (0, creatures)
-todo = [initial_state]
+todo = [(0, creatures)]
 seen = {creatures: 0}
 
 best = None
